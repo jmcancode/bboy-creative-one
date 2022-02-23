0eabfa7f04e3b0c55a03da9336216ec460a7bc18
Jon-Michael <narvaez.jonmichael@gmail.com>
2022-02-23 11:42:20 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+// custom components
+import backgroundVideo from "./assets/bboy.mp4"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <header className="App-header">
+                <video loop autoPlay muted id="video">
+                    <source src={backgroundVideo} type="video/mp4"/>
+                </video>
+                <div>
+                    <h1 className="App-title">bboy creative</h1>
+                </div>
+              
+            </header>
+        </div>
+    );
 }
 
 export default App;
